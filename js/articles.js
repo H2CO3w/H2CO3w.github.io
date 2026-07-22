f841d8a68620357ef9a27a52aa80bd7bda1a5494
Carbonicwww <112765712+H2CO3w@users.noreply.github.com>
2026-07-22 16:42:13 +0800

    Add files via upload

---
+++ js/articles.js
@@ -0,0 +1,10 @@
+const articles = [
+        {
+        category: 'tech',
+        tag: '置顶',
+        date: '2026-06-10',
+        title: 'MC机械动力蓝图集',
+        excerpt: ' 包含Minecraft机械动力模组蓝图',
+        link: 'posts/post1a.html'
+    },
+];
